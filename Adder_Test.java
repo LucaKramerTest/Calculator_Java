afe299dd3b3a24286862c59acf4f3e4a4d8be18a
LucaKramerTest <luca.kramer@web.de>
2024-09-17 14:19:57 +0200

    Create Adder_Test.java

---
+++ Adder_Test.java
@@ -0,0 +1,9 @@
+public class Adder_Test{
+  public void testAdd() {
+        Calculator calculator = new Adder();
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(-1, calculator.add(-2, 1));
+        assertEquals(0, calculator.add(0, 0));
+        assertEquals(-5, calculator.add(-2, -3));
+    }
+}
