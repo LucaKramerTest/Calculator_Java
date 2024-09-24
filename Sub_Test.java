7d58920535bbe504d445c03dac649562bff4cc6e
LucaKramerTest <luca.kramer@web.de>
2024-09-24 09:17:06 +0200

    Update Sub_Test.java

--- Sub_Test.java
+++ Sub_Test.java
@@ -1,6 +1,6 @@
 public class Sub_Test{
   public void testSubtractor() {
-        Adder calculator = new Adder();
+        Sub calculator = new Sub();
         assertEquals(5, calculator.subtractor(2, 3));
         assertEquals(-1, calculator.subtractor(-2, 1));
         assertEquals(0, calculator.subtractor(0, 0));
